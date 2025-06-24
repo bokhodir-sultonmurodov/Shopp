a23c1fe62b9c08c5731f7b0cdee2d2d642a646ae
bokhodir-sultonmurodov <boxodirsultanmurodov@gmail.com>
2025-06-24 16:45:15 +0500

    Wishlist

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,24 +1,28 @@
 import Products from '@/components/products/Products';
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Empty } from 'antd'; // ⬅️ Antd dan Empty import qilingan
 
 const Wishlist = () => {
-  const wishlist = useSelector((state) => state.wishlist.value)
+  const wishlist = useSelector((state) => state.wishlist.value);
   console.log(wishlist);
-  
+
   return (
     <div>
-      <h2>Wishlist</h2>
-      {
-        wishlist.length ?
-        <Products data={wishlist}/>
-        :
-        <div>
-          <img src="https://uzum.uz/static/img/hearts.cf414be.png" alt="" />
-        </div>
-      }
+      {wishlist.length ? (
+        <Products data={wishlist} />
+      ) : (
+        <Empty
+          image={Empty.PRESENTED_IMAGE_SIMPLE}
+          description={
+            <span className="text-gray-500 text-sm">
+              Wish List hozircha bo‘sh
+            </span>
+          }
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Wishlist
+export default Wishlist;
